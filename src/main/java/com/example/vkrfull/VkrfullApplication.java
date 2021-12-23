076947106ed6862eb898bcfd9142acea95e7d78a
Vikki111 <osnova_forall@mail.ru>
2021-12-23 18:29:53 +0400

    Initial commit

---
+++ src/main/java/com/example/vkrfull/VkrfullApplication.java
@@ -0,0 +1,13 @@
+package com.example.vkrfull;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VkrfullApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VkrfullApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/vkrfull/VkrfullApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vkrfull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VkrfullApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
